9a152a69b4229e4c754446e6893a2fa0c94eeaf7
luqmanadi <78971486+luqmanadi@users.noreply.github.com>
2023-05-30 15:59:35 +0700

    layout dashboard User dan Admin
    file index.php admin dan user diedit dan layoutnya

--- layout/bottom.php
+++ layout/bottom.php
@@ -33,7 +33,7 @@
             <div class="modal-body">Pilih "Logout" jika ingin keluar.</div>
             <div class="modal-footer">
                 <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                <a class="btn btn-primary" href="login.html">Logout</a>
+                <a class="btn btn-primary" href="../logout.php">Logout</a>
             </div>
         </div>
     </div>
--- layout/sidenav.php
+++ layout/sidenav.php
@@ -2,7 +2,7 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?= str_contains($_SERVER['PHP_SELF'], '/admin') ? '../admin/index.php' : '../user/index.php' ?>">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
@@ -14,7 +14,7 @@
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item active">
-        <a class="nav-link" href="index.html">
+        <a class="nav-link" href="<?= str_contains($_SERVER['PHP_SELF'], '/admin') ? '../admin/index.php' : '../user/index.php' ?>">
             <i class="fas fa-fw fa-tachometer-alt"></i>
             <span>Dashboard</span></a>
     </li>
@@ -28,41 +28,51 @@
     </div>
     
     <!-- Nav Item - User Collapse Menu -->
+    <?php if(str_contains($_SERVER['PHP_SELF'],'/admin') ): ?>
+        <li class="nav-item" >
+            <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUser" aria-expanded="true" aria-controls="collapseUser">
+                <i class="fas fa-users fa-cog"></i>
+                <span>User</span>
+            </a>
+            <div id="collapseUser" class="collapse" aria-labelledby="headingUser" data-parent="#accordionSidebar">
+                <div class="bg-white py-2 collapse-inner rounded">
+                    <h6 class="collapse-header">Daftar User :</h6>
+                    <a class="collapse-item" href="../admin/dosen/index.php">Dosen</a>
+                    <a class="collapse-item" href="../admin/ormawa/index.php">Ormawa</a>
+                    <a class="collapse-item" href="../admin/mahasiswa/index.php">Mahasiswa</a>
+                </div>
+            </div>
+        </li>
+    <?php endif ?>
+    
+
+    <!-- Nav Item - Ruangan Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-users fa-cog"></i>
-            <span>User</span>
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" aria-expanded="true" aria-controls="collapseRuangan" data-target="#collapseRuangan"  >
+            <i class="fas fa-fw fa-door-open"></i>
+            <span>Ruangan</span>
         </a>
-        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+        <div  id="collapseRuangan" class="collapse" data-parent="#accordionSidebar" aria-labelledby="headingRuangan" >
             <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Daftar User:</h6>
-                <a class="collapse-item" href="buttons.html">Dosen</a>
-                <a class="collapse-item" href="cards.html">Ormawa</a>
-                <a class="collapse-item" href="cards.html">Mahasiswa</a>
+                <a class="collapse-item" href="<?= str_contains($_SERVER['PHP_SELF'], '/admin') ? '../admin/ruangan/index.php' : '../user/ruangan.php' ?>">List</a>
+                <?php if(str_contains($_SERVER['PHP_SELF'],'/admin') ): ?>
+                    <a class="collapse-item" href="../admin/ruangan/create.php">Tambah Ruang</a>
+                <?php endif ?>
             </div>
         </div>
     </li>
 
-    <!-- Nav Item - Ruangan Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"aria-expanded="true" aria-controls="collapseUtilities">
-            <i class="fas fa-fw fa-door-open"></i>
-            <span>Ruangan</span>
-        </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
-            
-        </div>
-    </li>
-
 
     <!-- Nav Item - Ruangan Collapse Menu -->
     <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePeminjaman" aria-expanded="true" aria-controls="collapsePeminjaman">
             <i class="fas fa-fw fa-file"></i>
             <span>Riwayat Peminjaman</span>
         </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            
+        <div id="collapsePeminjaman" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <a class="collapse-item" href="<?= str_contains($_SERVER['PHP_SELF'], '/admin') ? '../admin/peminjaman/index.php' : '../user/peminjaman.php'?>">List</a>
+            </div>
         </div>
     </li>
     
