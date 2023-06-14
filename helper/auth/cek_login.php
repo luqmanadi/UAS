53a9d227ec868f2feb3be1b87336556a25afad0b
tataamal <122243912+tataamal@users.noreply.github.com>
2023-06-14 14:08:49 +0700

    push lagi

--- helper/auth/cek_login.php
+++ helper/auth/cek_login.php
@@ -24,7 +24,7 @@ if (isset($_POST['login'])) {
             header('Location: ../../user/ormawa/index.php');
         } elseif ($_SESSION['level'] == 4) {
             header('Location: ../../user/mahasiswa/index.php');
-        }
+        } 
     } else {
         header('Location: ../../login.php?loginFail=1');
     }
